aee0b614e7ee11b9e717d342ee69a8170385a7b3
mrjumpyy <remryuck.a7x@gmail.com>
2016-12-25 11:53:53 +0000

    AreaCalculator

---
+++ Default.aspx
@@ -0,0 +1,117 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+
+    <h1 style="color:whitesmoke">Hello and Welcome to the Area Calculator!</h1>
+
+        <br /><br />
+
+        <h3 style="color:whitesmoke">Please Enter the Values and Click the Calculate Button:</h3>
+        
+        <br /><br />
+
+    <div class="col-md-6 leftSide">
+        <%-- Display for Rectangle Calculator --%>
+         <asp:Label ID="LabelRectangle" runat="server" Text="Rectangle " Font-Bold="True" Font-Size="Medium" ForeColor="WhiteSmoke"></asp:Label>
+        <img src="Content/Pictures/rectangle.jpg.gif" />
+        <br />
+        <h5 style="color:whitesmoke">Length: </h5>
+        <asp:TextBox ID="RectangleLength" runat="server"></asp:TextBox>
+        <asp:DropDownList ID="LengthList" runat="server">
+            <asp:ListItem>Millimeter</asp:ListItem>
+            <asp:ListItem>Centimeter</asp:ListItem>
+            <asp:ListItem>Meter</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <h5 style="color:whitesmoke">Height: </h5><asp:TextBox ID="RectangleHeight" runat="server"></asp:TextBox>
+        <asp:DropDownList ID="HeightList" runat="server">
+            <asp:ListItem>Millimeter</asp:ListItem>
+            <asp:ListItem>Centimeter</asp:ListItem>
+            <asp:ListItem>Meter</asp:ListItem>
+        </asp:DropDownList>
+        <br /><br />
+        <asp:Button ID="RectangleButton" runat="server" Text="Calculate" OnClick="RectangleButton_Click" CssClass="btn-success"/>
+        <br /><br />
+        <asp:Label ID="RectangleReponse" runat="server" ForeColor="WhiteSmoke" Font-Bold="true"></asp:Label>
+        <br /><br />
+
+        <%-- Display For Triangle Calculator --%>
+        <asp:Label ID="LabelTriangle" runat="server" Text="Triangle " Font-Bold="True" Font-Size="Medium" ForeColor="WhiteSmoke"></asp:Label>
+        <img src="Content/Pictures/triangle.gif" height="70" width="80"/>
+        <br />
+        <h5 style="color:whitesmoke">Base: </h5>
+        <asp:TextBox ID="TriangleBase" runat="server"></asp:TextBox>
+        <asp:DropDownList ID="TriangleDropDownBase" runat="server">
+            <asp:ListItem>Millimeter</asp:ListItem>
+            <asp:ListItem>Centimeter</asp:ListItem>
+            <asp:ListItem>Meter</asp:ListItem>
+        </asp:DropDownList>
+        <h5 style="color:whitesmoke">Height: </h5>
+        <asp:TextBox ID="TriangleHeight" runat="server"></asp:TextBox>
+        <asp:DropDownList ID="TriangleDropDownHeight" runat="server">
+            <asp:ListItem>Millimeter</asp:ListItem>
+            <asp:ListItem>Centimeter</asp:ListItem>
+            <asp:ListItem>Meter</asp:ListItem>
+        </asp:DropDownList>
+        <br /><br />
+        <asp:Button ID="TriangleButton" runat="server" Text="Calculate" OnClick="TriangleButton_Click" CssClass="btn-success" />
+        <br /><br />
+        <asp:Label ID="TriangleResponse" runat="server" ForeColor="WhiteSmoke" Font-Bold="true"></asp:Label>    
+            <br /><br />
+    </div>
+
+    <div class="col-md-6 rightSide">
+        <%-- Display for Trapezoid --%>
+        <asp:Label ID="LabelTrapezoid" runat="server" Text="Trapezoid " Font-Bold="True" Font-Size="Medium" ForeColor="WhiteSmoke"></asp:Label>
+        <img src="Content/Pictures/trapezoid.gif" />
+        <br />
+        <h5 style="color:whitesmoke">Top Width: </h5>
+        <asp:TextBox ID="TrapTopLength" runat="server"></asp:TextBox>
+        <asp:DropDownList ID="TrapezoidTopWidth" runat="server">
+            <asp:ListItem>Millimeter</asp:ListItem>
+            <asp:ListItem>Centimeter</asp:ListItem>
+            <asp:ListItem>Meter</asp:ListItem>
+        </asp:DropDownList>
+
+        <h5 style="color:whitesmoke">Bottom Width: </h5>
+        <asp:TextBox ID="TrapBottomLength" runat="server"></asp:TextBox>
+        <asp:DropDownList ID="TrapezoidBottomWidth" runat="server">
+            <asp:ListItem>Millimeter</asp:ListItem>
+            <asp:ListItem>Centimeter</asp:ListItem>
+            <asp:ListItem>Meter</asp:ListItem>
+        </asp:DropDownList>
+
+        <h5 style="color:whitesmoke">Height: </h5>
+        <asp:TextBox ID="TrapHeight" runat="server"></asp:TextBox>
+        <asp:DropDownList ID="TrapezoidHeight" runat="server">
+            <asp:ListItem>Millimeter</asp:ListItem>
+            <asp:ListItem>Centimer</asp:ListItem>
+            <asp:ListItem>Meter</asp:ListItem>
+        </asp:DropDownList>
+        <br /><br />
+        <asp:Button ID="TrapezoidButton" runat="server" Text="Calculate" OnClick="TrapezoidButton_Click" CssClass="btn-success"/>
+        <br /><br />
+        <asp:Label ID="TrapezoidResponse" runat="server" ForeColor="WhiteSmoke" Font-Bold="true"></asp:Label>
+        <br /><br />
+
+        <%-- Display for circle! --%>
+
+        <asp:Label ID="Label1" runat="server" Text="Circle " font-bold="true" Font-Size="Medium" ForeColor="WhiteSmoke"></asp:Label>
+        <img src="Content/Pictures/circle.gif" height="65" width="65"/>
+        <br />
+        <h5 style="color:whitesmoke">Radius: </h5>
+        <asp:TextBox ID="CircleRadius" runat="server"></asp:TextBox>
+        <asp:DropDownList ID="RadiusDropDown" runat="server">
+            <asp:ListItem>Millimeter</asp:ListItem>
+            <asp:ListItem>Centimeter</asp:ListItem>
+            <asp:ListItem>Meter</asp:ListItem>
+        </asp:DropDownList>
+        <br /><br />
+        <asp:Button ID="CircleButton" runat="server" Text="Calculate" OnClick="CircleButton_Click" CssClass="btn-success"/>
+        <br /><br /><br />
+        <asp:Label ID="CircleResponse" runat="server" ForeColor="WhiteSmoke" Font-Bold="true"></asp:Label>        
+        <br /><br />
+    </div>    
+    
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void RectangleButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string length = RectangleLength.Text;
+            string height = RectangleHeight.Text;
+            double lengthAsDouble = Convert.ToDouble(length);
+            double heightAsDouble = Convert.ToDouble(height);
+
+            if (LengthList.SelectedValue == "Millimeter" && HeightList.SelectedValue == "Millimeter")
+            {
+                RectangleReponse.Text = "The Area of Your Rectangle is: " + lengthAsDouble * heightAsDouble + "mm\xB2.";
+            }
+            else if (LengthList.SelectedValue == "Centimeter" && HeightList.SelectedValue == "Centimeter")
+            {
+                RectangleReponse.Text = "The Area of Your Rectangle is: " + lengthAsDouble * heightAsDouble + "cm\xB2.";
+            }
+            else if (LengthList.SelectedValue == "Meter" && HeightList.SelectedValue == "Meter")
+            {
+                RectangleReponse.Text = "The Area of Your Rectangle is: " + lengthAsDouble * heightAsDouble + "m\xB2.";
+            }
+            else
+            {
+                RectangleReponse.Text = "Please Choose the Same Unit of Measurement!";
+            }
+        }
+        catch (FormatException)
+        {
+            RectangleReponse.Text = "Error! Please Enter a Valid Value! E.g. 5, 25.55";
+        }
+    }
+
+    protected void TriangleButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string _base = TriangleBase.Text;
+            string _height = TriangleHeight.Text;
+            double _baseAsDouble = Convert.ToDouble(_base);
+            double _heightAsDouble = Convert.ToDouble(_height);
+
+            if (TriangleDropDownBase.SelectedValue == "Millimeter" && TriangleDropDownHeight.SelectedValue == "Millimeter")
+            {
+                TriangleResponse.Text = "The Area of Your Rectangle is: " + (_baseAsDouble * _heightAsDouble) / 2 + "mm\xB2.";
+            }
+            else if (TriangleDropDownBase.SelectedValue == "Centimeter" && TriangleDropDownHeight.SelectedValue == "Centimeter")
+            {
+                RectangleReponse.Text = "The Area of Your Rectangle is: " + (_baseAsDouble * _heightAsDouble) / 2 + "cm\xB2.";
+            }
+            else if (TriangleDropDownBase.SelectedValue == "Meter" && TriangleDropDownHeight.SelectedValue == "Meter")
+            {
+                RectangleReponse.Text = "The Area of Your Rectangle is: " + (_baseAsDouble * _heightAsDouble) / 2 + "m\xB2.";
+            }
+            else
+            {
+                RectangleReponse.Text = "Please Choose the Same Unit of Measurement!";
+            }
+        }
+        catch (FormatException)
+        {
+            TriangleResponse.Text = "Error! Please Enter a Valid Value! E.g. 5, 25.55";
+        }
+    }
+
+    protected void TrapezoidButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string trapWidth1 = TrapTopLength.Text;
+            string trapwidth2 = TrapBottomLength.Text;
+            string trapHeight = TrapHeight.Text;
+            double width1AsDouble = Convert.ToDouble(trapWidth1);
+            double width2AsDouble = Convert.ToDouble(trapwidth2);
+            double trapHeightAsDouble = Convert.ToDouble(trapHeight);
+
+            if (TrapezoidTopWidth.Text == "Millimeter" && TrapezoidBottomWidth.Text == "Millimeter" && TrapezoidHeight.Text == "Millimeter")
+            {
+                TrapezoidResponse.Text = "The Area of Your Trapezoid is: " + ((width1AsDouble + width2AsDouble) / 2) * trapHeightAsDouble + "mm\xB2";
+            }
+            else if (TrapezoidTopWidth.Text == "Centimeter" && TrapezoidBottomWidth.Text == "Centimeter" && TrapezoidHeight.Text == "Centimeter")
+            {
+                TrapezoidResponse.Text = "The Area of Your Trapezoid is: " + ((width1AsDouble + width2AsDouble) / 2) * trapHeightAsDouble + "cm\xB2";
+            }
+            else if (TrapezoidTopWidth.Text == "Meter" && TrapezoidBottomWidth.Text == "Meter" && TrapezoidHeight.Text == "Meter")
+            {
+                TrapezoidResponse.Text = "The Area of Your Trapezoid is: " + ((width1AsDouble + width2AsDouble) / 2) * trapHeightAsDouble + "m\xB2";
+            }
+            else
+            {
+                TrapezoidResponse.Text = "Please Choose the Same Unit of Measurement!";
+            }
+
+        }
+        catch (FormatException)
+        {
+            TrapezoidResponse.Text = "Error! Please Enter a Valid Value! E.g. 5, 25.55!";
+        }
+    }
+
+    protected void CircleButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string radius = CircleRadius.Text;
+            double radiusAsDouble = Convert.ToDouble(radius);
+
+            if (RadiusDropDown.SelectedValue == "Millimeter")
+            {
+                CircleResponse.Text = "The Area of Your Circle is: " + Math.PI * (radiusAsDouble * radiusAsDouble) + "mm\xB2";
+            }
+            else if (RadiusDropDown.SelectedValue == "Centimeter")
+            {
+                CircleResponse.Text = "The Area of Your Circle is: " + Math.PI * (radiusAsDouble * radiusAsDouble) + "cm\xB2";
+            }
+            else if (RadiusDropDown.SelectedValue == "Meter")
+            {
+                CircleResponse.Text = "The Area of Your Circle is: " + Math.PI * (radiusAsDouble * radiusAsDouble) + "m\xB2";
+            }
+            else
+            {
+                CircleResponse.Text = "Please Choose the Same Unit of Measurement!";
+            }
+        }
+        catch (FormatException)
+        {
+            CircleResponse.Text = "Error! Please Enter a Valid Value!E.g. 5, 25.55!";
+        }
+    }
+}
